44625fffd7467c5d0e822da6b1349cbd8d471479
Krishna <krishnasritarun95@gmail.com>
2025-06-29 20:28:56 +0530

    add user problem solved

--- Backend/routes/Seller.Route.js
+++ Backend/routes/Seller.Route.js
@@ -4,7 +4,7 @@ const wrapAsync = require('../utils/wrapAsync');
 const SellerControllers= require("../controllers/Seller.Controller");
 const { registerSellerMiddleware, loginValidation, authMiddleware, authorizeRoles } = require('../middleware');
 
-router.post("/register",authorizeRoles("ROLE_ADMIN"),registerSellerMiddleware,wrapAsync(SellerControllers.register))
+router.post("/register",authMiddleware,authorizeRoles("ROLE_ADMIN"),registerSellerMiddleware,wrapAsync(SellerControllers.register))
 router.post("/login",loginValidation,wrapAsync(SellerControllers.login));
 router.get("/user",authMiddleware,authorizeRoles("ROLE_ADMIN"),wrapAsync(SellerControllers.usersData));
 
