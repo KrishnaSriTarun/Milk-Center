02c18ddf627fc3873802b0a026f3c87dda6b6475
Krishna <krishnasritarun95@gmail.com>
2025-11-01 10:26:07 +0530

    update rates bug fix

--- Backend/routes/Rate.Route.js
+++ Backend/routes/Rate.Route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const rateController = require('../controllers/Rate.Controller');
 const  wrapAsync  = require('../utils/wrapAsync'); 
-const { validateAddRate, validateUpdateRate,authorizeRoles } = require('../middleware');
+const { validateAddRate, validateUpdateRate,authorizeRoles, authMiddleware } = require('../middleware');
 
 router.get('/getRate', wrapAsync(rateController.getRates));
 // router.post('/addRate', validateAddRate, wrapAsync(rateController.addRate));
-router.put('/updateRate/:id', validateUpdateRate,authorizeRoles("ROLE_ADMIN"), wrapAsync(rateController.updateRate));
+router.put('/updateRate/:id',authMiddleware, validateUpdateRate,authorizeRoles("ROLE_ADMIN"), wrapAsync(rateController.updateRate));
 
 module.exports = router;
